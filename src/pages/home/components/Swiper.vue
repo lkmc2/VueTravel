2c565033b47b00b72f21531a68065ee01b999dd8
lkmc2 <lkmc2@163.com>
2019-01-22 22:38:24 +0800

    使用循环生成轮播图

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="wrapper">
     <swiper :options="swiperOption">
-      <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1811/a8/cb43c4ac6c215d02.jpg_750x200_83dee994.jpg">
+      <!-- 遍历生成轮播图 -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/e9b6ede83785c7673eb1bcda5a78f123.jpg_750x200_0e6d0de9.jpg">
-      </swiper-slide>
+
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -19,11 +17,30 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: { }
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/a8/cb43c4ac6c215d02.jpg_750x200_83dee994.jpg'
+        }, {
+          id: '0002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/e9b6ede83785c7673eb1bcda5a78f123.jpg_750x200_0e6d0de9.jpg'
+        }
+      ]
     }
   }
 }
 </script>
+
+<!--声明全局样式，覆盖轮播图组件的指示点颜色-->
+<style>
+  .wrapper .swiper-pagination-bullet-active {
+    background: #fff;
+  }
+</style>
 
 <style lang="scss" scoped>
   .wrapper {
