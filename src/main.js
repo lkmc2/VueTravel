e8f683035a60faf86edd6231d94bb51566e2340e
lkmc2 <lkmc2@163.com>
2019-01-22 21:15:22 +0800

    引入轮播图组件

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper' // 轮播图组件
 import './assets/styles/reset.css' // 页面重置样式
 import './assets/styles/border.css' // 防止1像素边框问题
 import './assets/styles/iconfont.css' // 图标字体
+import 'swiper/dist/css/swiper.css' // 轮播图组件的样式
 
 Vue.config.productionTip = false
 // 启用fastClick，解决点击事件300毫秒延迟问题
 fastClick.attach(document.body)
+
+// 启用轮播图组件
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
