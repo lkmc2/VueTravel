5576e031aaff074be9e2f3b1cfdcecf7595e0a28
lkmc2 <lkmc2@163.com>
2019-01-22 19:47:19 +0800

    使用scss编写Header组件

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>This is a header</div>
+    <div class="header">
+      <div class="header-left">返回</div>
+      <div class="header-input">输入城市/景点/游玩主题</div>
+      <div class="header-right">城市</div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +12,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<!--使用scss编写样式，1rem = 50px-->
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    line-height: .86rem;
+    background-color: #00bcd4;
+    color: #fff;
 
+    .header-left {
+      width: .64rem;
+      float: left;
+    }
+
+    .header-input {
+      flex: 1;
+      height: .64rem;
+      line-height: .64rem;
+      margin-top: .12rem;
+      margin-left: .2rem;
+      background: #fff;
+      border-radius: .1rem;
+      color: #ccc;
+    }
+
+    .header-right {
+      width: 1.24rem;
+      float: right;
+      text-align: center;
+    }
+  }
 </style>
