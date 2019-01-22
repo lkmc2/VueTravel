d1afed6f1c9860b49792686a12cb0a74a9eb8a5b
lkmc2 <lkmc2@163.com>
2019-01-22 18:50:17 +0800

    引入页面重置样式

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <!--template的根元素只能有一个-->
   <div>
-    <div class="home">never</div>
+    <div class="home">home</div>
     <!--跳转到/list对应路由的页面-->
     <router-link to="/list" class="home">跳转</router-link>
   </div>
