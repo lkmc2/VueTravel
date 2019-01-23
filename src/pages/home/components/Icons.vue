c81fd5159fd97ae58b37ed18bcf530932a2d3146
lkmc2 <lkmc2@163.com>
2019-01-23 11:05:21 +0800

    为图标网格添加页面滑动效果

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,54 +1,28 @@
 <template>
     <!--图标网格-->
     <div class="icons">
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
+      <!--轮播图组件-->
+      <swiper>
+        <!--轮播图页面1-->
+        <swiper-slide>
+          <div class="icon">
+            <div class="icon-img">
+              <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+          </div>
+        </swiper-slide>
+
+        <!--轮播图页面2-->
+        <swiper-slide>
+          <div class="icon">
+            <div class="icon-img">
+              <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+            </div>
+            <p class="icon-desc">热门景点</p>
+          </div>
+        </swiper-slide>
+      </swiper>
     </div>
 </template>
 
@@ -58,47 +32,49 @@ export default {
 }
 </script>
 
+<!--全局样式，设置轮播图组件的高度与图标网格一样高-->
+<style>
+  .icons .swiper-container {
+    height: 0;
+    padding-bottom: 50%;
+  }
+</style>
+
 <style lang="scss" scoped>
   @import '../../../assets/styles/variables';
 
-  .icons {
+  .icon {
+    position: relative;
     overflow: hidden;
+    float: left;
+    width: 25%;
     height: 0;
-    padding-bottom: 50%;
+    padding-bottom: 25%;
 
-    .icon {
-      position: relative;
-      overflow: hidden;
-      float: left;
-      width: 25%;
-      height: 0;
-      padding-bottom: 25%;
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: .44rem;
+      box-sizing: border-box;
+      padding: .1rem;
 
-      .icon-img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: .44rem;
-        box-sizing: border-box;
-        padding: .1rem;
-
-        .icon-img-content {
-          display: block;
-          margin: 0 auto;
-          height: 100%;
-        }
+      .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
       }
+    }
 
-      .icon-desc {
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        line-height: .44rem;
-        text-align: center;
-        color: $darkTextColor;
-      }
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      line-height: .44rem;
+      text-align: center;
+      color: $darkTextColor;
     }
   }
 </style>
