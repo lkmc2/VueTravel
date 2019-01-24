fd94a2d32bcdc5b8f04538153b568d89a2392546
lkmc2 <lkmc2@163.com>
2019-01-24 22:59:51 +0800

    为侧边栏字母表组件的触摸事件优化性能

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -24,8 +24,14 @@ export default {
   },
   data () {
     return {
-      touchStatus: false // 触摸状态
+      touchStatus: false, // 触摸状态
+      startY: 0, // A字母距离Alphabet组件顶部的纵坐标
+      timer: null // 计时器
     }
+  },
+  // 页面数据更新时调用此方法
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   computed: {
     // 获取侧边栏出现的字母数组
@@ -50,20 +56,26 @@ export default {
     // 侧边栏触摸移动事件
     handleTouchMove (event) {
       if (this.touchStatus) {
-        // 获取A字母距离Alphabet组件顶部的纵坐标
-        const startY = this.$refs['A'][0].offsetTop
-        // 获取触摸屏幕时，距离Alphabet组件顶部的纵坐标，
-        // 79是Header和Search组件的高，因为触摸事件是算整个屏幕的
-        const touchY = event.touches[0].clientY - 79
-
-        // 字母的下标，20是每个字母的高度
-        const index = Math.floor((touchY - startY) / 20)
-
-        // 下标范围需在26个字母的范围内
-        if (index >= 0 && index < this.letters.length) {
-          // 触发change事件给父组件接收，参数为下标对应的字母
-          this.$emit('change', this.letters[index])
+        // 如果已存在计时器，则清除
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+
+        // 创建计时器，并设置16毫秒后再执行，减少触摸事件造成的资源浪费
+        this.timer = setTimeout(() => {
+          // 获取触摸屏幕时，距离Alphabet组件顶部的纵坐标，
+          // 79是Header和Search组件的高，因为触摸事件是算整个屏幕的
+          const touchY = event.touches[0].clientY - 79
+
+          // 字母的下标，20是每个字母的高度
+          const index = Math.floor((touchY - this.startY) / 20)
+
+          // 下标范围需在26个字母的范围内
+          if (index >= 0 && index < this.letters.length) {
+            // 触发change事件给父组件接收，参数为下标对应的字母
+            this.$emit('change', this.letters[index])
+          }
+        })
       }
     },
     // 侧边栏触摸结束事件
