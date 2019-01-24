38f6b56c072fe626348db2111a21269277d45f26
lkmc2 <lkmc2@163.com>
2019-01-24 18:05:05 +0800

    编写字母对应的城市列表样式

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -7,6 +7,14 @@
         <div class="button-wrapper">
           <div class="button">北京</div>
         </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
         <div class="button-wrapper">
           <div class="button">北京</div>
         </div>
@@ -22,10 +30,52 @@
       </div>
     </div>
     <div class="area">
-      <div class="title border-topbottom">热门城市</div>
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
     </div>
     <div class="area">
       <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,28 +96,51 @@ export default {
     }
   }
 
-  .title {
-    line-height: .44rem;
-    background: #eee;
-    padding-left: .2rem;
-    color: #666;
-    font-size: .26rem;
+  .border-bottom {
+    &:before {
+      border-color: #ccc;
+    }
   }
 
-  .button-list {
+  .list {
     overflow: hidden;
-    padding: .1rem .6rem .1rem .1rem;
+    position: absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
 
-    .button-wrapper {
-      float: left;
-      width: 33.33%;
+    .title {
+      line-height: .44rem;
+      background: #eee;
+      padding-left: .2rem;
+      color: #666;
+      font-size: .26rem;
+    }
 
-      .button {
-        margin: .1rem;
-        padding: .1rem 0;
-        text-align: center;
-        border: .02rem solid #ccc;
-        border-radius: .06rem;
+    .button-list {
+      overflow: hidden;
+      padding: .1rem .6rem .1rem .1rem;
+
+      .button-wrapper {
+        float: left;
+        width: 33.33%;
+
+        .button {
+          margin: .1rem;
+          padding: .1rem 0;
+          text-align: center;
+          border: .02rem solid #ccc;
+          border-radius: .06rem;
+        }
+      }
+    }
+
+    .item-list {
+      .item {
+        line-height: .76rem;
+        color: #666;
+        padding-left: .2rem;
       }
     }
   }
