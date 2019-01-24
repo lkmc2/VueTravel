64b058bb8cc5155a1eefed22e0393268dd865f70
lkmc2 <lkmc2@163.com>
2019-01-24 17:33:45 +0800

    编写城市选择页的Search搜索框组件

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
       <city-header></city-header>
+      <city-search></city-search>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header' // 页面头部
+import CitySearch from './components/Search' // 搜索框
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
