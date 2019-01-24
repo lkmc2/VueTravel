1d5a85a6c261b6fc668e6a8867672b029f04ada1
lkmc2 <lkmc2@163.com>
2019-01-24 22:49:27 +0800

    编写侧边栏字母表组件滚动时与城市列表的联动事件

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,12 +1,16 @@
 <template>
-    <!--侧边栏字母表组件-->
+    <!--侧边栏字母表组件，ref表示当前dom的引用名称-->
     <ul class="list">
       <li
           class="item"
-          v-for="(item, key) of cities"
-          :key="key"
+          v-for="item of letters"
+          :key="item"
+          :ref="item"
+          @touchstart="handleTouchStart"
+          @touchmove="handleTouchMove"
+          @touchend="handleTouchEnd"
           @click="handleLetterClick">
-        {{key}}
+        {{item}}
       </li>
     </ul>
 </template>
@@ -18,11 +22,53 @@ export default {
   props: {
     cities: Object
   },
+  data () {
+    return {
+      touchStatus: false // 触摸状态
+    }
+  },
+  computed: {
+    // 获取侧边栏出现的字母数组
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
   methods: {
     // 处理侧边字母栏的点击事件
     handleLetterClick (event) {
       // Alphabet组件触发change事件，事件参数为点击的字母
       this.$emit('change', event.target.innerText)
+    },
+    // 侧边栏触摸起始事件
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    // 侧边栏触摸移动事件
+    handleTouchMove (event) {
+      if (this.touchStatus) {
+        // 获取A字母距离Alphabet组件顶部的纵坐标
+        const startY = this.$refs['A'][0].offsetTop
+        // 获取触摸屏幕时，距离Alphabet组件顶部的纵坐标，
+        // 79是Header和Search组件的高，因为触摸事件是算整个屏幕的
+        const touchY = event.touches[0].clientY - 79
+
+        // 字母的下标，20是每个字母的高度
+        const index = Math.floor((touchY - startY) / 20)
+
+        // 下标范围需在26个字母的范围内
+        if (index >= 0 && index < this.letters.length) {
+          // 触发change事件给父组件接收，参数为下标对应的字母
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    // 侧边栏触摸结束事件
+    handleTouchEnd () {
+      this.touchStatus = false
     }
   }
 }
