5ad5bd66dec76234ce6d28df93fa0a94930a5fb8
lkmc2 <lkmc2@163.com>
2019-01-25 19:42:19 +0800

    使用<keep-alive>标签优化对路由界面的切换消耗

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <!-- router-view显示的是当前地址所对应的内容 -->
-    <router-view/>
+    <!--当路由上的内容被加载过之后，放入内存中，
+    下一次在打开该路由，不需要重新加载组件，防止每次切换路由都进行ajax请求-->
+    <keep-alive>
+      <!-- router-view显示的是当前地址所对应的内容 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
