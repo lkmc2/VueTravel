c9d46724069a85696cdaa7f945ec2e1d03e60e57
lkmc2 <lkmc2@163.com>
2019-01-25 19:04:14 +0800

    修复首页Header因城市名过长引起的样式问题

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -58,7 +58,8 @@ export default {
     }
 
     .header-right {
-      width: 1.24rem;
+      min-width: 1.04rem;
+      padding: 0 .1rem;
       float: right;
       text-align: center;
       color: #fff;
