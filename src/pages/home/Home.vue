2fe3a89afa7561afec32780521eb50f144ce7352
lkmc2 <lkmc2@163.com>
2019-01-25 20:00:18 +0800

    使用activated函数，在当前城市切换后，重新请求首页数据

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -17,6 +17,7 @@ import HomeIcons from './components/Icons' // 图标网格
 import HomeRecommend from './components/Recommend' // 热销推荐模块
 import HomeWeekend from './components/Weekend' // 周末去哪儿模块
 import axios from 'axios' // 网络请求工具，统一在首页请求数据，然后传到子组件中
+import { mapState } from 'vuex' // Vuex状态获取器
 
 export default {
   name: 'Home',
@@ -30,16 +31,22 @@ export default {
   },
   data () {
     return {
-      swiperList: [],
-      iconList: [],
-      recommendList: [],
-      weekendList: []
+      lastCity: '', // 上一次选择的城市
+      swiperList: [], // 轮播图列表
+      iconList: [], // 图标网格列表
+      recommendList: [], // 推荐列表
+      weekendList: [] // 周末去哪儿列表
     }
   },
+  computed: {
+    // 将Vuex中state的city的获取方法设置到计算属性中，可以直接使用this.city获取当前城市信息
+    ...mapState(['city'])
+  },
   methods: {
+    // 请求首页数据
     getHomeInfo () {
       // axios请求json数据，然后将数据传给getHomeInfoSucc方法
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city' + this.city)
         .then(this.getHomeInfoSucc)
     },
     // 处理json数据
@@ -57,7 +64,19 @@ export default {
     }
   },
   mounted () {
+    // 记录本次选择的城市为上一次的城市
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  // 当App.vue中使用了<keep-alive>标签时，将可以使用activated
+  // 当页面进行切换，重新执行的时候，将会调用activated函数
+  activated () {
+    // 当上一次选择的城市与本次不相等时，重新请求首页数据
+    if (this.lastCity !== this.city) {
+      // 记录本次选择的城市为上一次的城市
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
