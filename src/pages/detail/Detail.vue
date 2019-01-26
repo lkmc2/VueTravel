be8b66d9e5a6424c69f30d4b809129c94f332f89
lkmc2 <lkmc2@163.com>
2019-01-26 12:14:01 +0800

    编写递归组件，展示票据信息

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -4,19 +4,47 @@
       <detail-banner></detail-banner>
       <detail-header></detail-header>
 
-      <div class="content"></div>
+      <div class="content">
+        <!--向子组件传递票据信息-->
+        <detail-list :list="list"></detail-list>
+      </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner' // 横幅广告组件
 import DetailHeader from './components/Header' // 头部组件
+import DetailList from './components/List' // 详情列表
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆联票',
+              children: [
+                { title: '成人三馆联票 - 大连连锁店销售' }
+              ]
+            },
+            {
+              title: '成人五馆联票'
+            }
+          ]
+        },
+        { title: '学生票' },
+        { title: '儿童票' },
+        { title: '特惠票' }
+      ]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,54 @@
+<template>
+    <!--详情页列表-->
+    <div>
+      <div class="item"
+           v-for="(item, index) of list"
+           :key="index"
+      >
+        <div class="item-title border-bottom">
+          <span class="item-title-icon"></span>
+          {{item.title}}
+        </div>
+
+        <!--当列表元素存在子元素时，调用<detail-list>标签自身生成内容，即递归组件-->
+        <div v-if="item.children" class="item-child">
+          <!--将item的children的值传给<detail-list>组件的list属性-->
+          <detail-list :list="item.children"></detail-list>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  // 从父组件接收的属性
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .item-title-icon {
+    position: relative;
+    left: .06rem;
+    top: .06rem;
+    display: inline-block;
+    width: .36rem;
+    height: .36rem;
+    background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+    margin-right: .1rem;
+    background-size: .4rem 3rem;
+  }
+
+  .item-title {
+    line-height: .8rem;
+    font-size: .32rem;
+    padding: 0 .2rem;
+  }
+
+  .item-child {
+    padding: 0 .2rem;
+  }
+</style>
