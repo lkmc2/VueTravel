5138c333594c8bdaaa42aedeee7811bc38de877d
lkmc2 <lkmc2@163.com>
2019-01-26 17:12:39 +0800

    使用异步组件，可以让项目按需加载组件，而不是一次性加载所有组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
+// 路由控制器
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home', // 主页
-      component: Home
+      // () => import('组件路径')表示加载异步组件，
+      // 可以让项目按需加载组件，而不是一次性加载所有组件，
+      // 从而达到减少生成的js文件的体积
+      component: () => import('@/pages/home/Home')
     },
     {
       path: '/city',
       name: 'City', // 城市信息页
-      component: City
+      component: () => import('@/pages/city/City')
     },
     {
       // 动态路由，:id表示该值可变
       path: '/detail/:id',
       name: 'Detail', // 详情页
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   // 设置页面切换时，始终回到页面顶部，防止页面联动时的影响
