64ae7066c0219a469391aa19a32915f061623d13
lkmc2 <lkmc2@163.com>
2019-01-26 17:59:03 +0800

    换回使用同步组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
-// 路由控制器
 export default new Router({
   routes: [
     {
@@ -12,18 +14,21 @@ export default new Router({
       // () => import('组件路径')表示加载异步组件，
       // 可以让项目按需加载组件，而不是一次性加载所有组件，
       // 从而达到减少生成的js文件的体积
-      component: () => import('@/pages/home/Home')
+      // 异步组件建议项目超过1M时才使用，
+      // 否则项目太小，分拆成多次请求，反而降低性能
+      // component: () => import('@/pages/home/Home')
+      component: Home
     },
     {
       path: '/city',
       name: 'City', // 城市信息页
-      component: () => import('@/pages/city/City')
+      component: City
     },
     {
       // 动态路由，:id表示该值可变
       path: '/detail/:id',
       name: 'Detail', // 详情页
-      component: () => import('@/pages/detail/Detail')
+      component: Detail
     }
   ],
   // 设置页面切换时，始终回到页面顶部，防止页面联动时的影响
